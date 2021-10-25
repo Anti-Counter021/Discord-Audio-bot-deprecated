7a61ab6f83b29fe4609de0284b1ba4b4ec1a83fb
counter021 <ara123rus123ara@gmail.com>
2021-10-25 15:51:21 +0300

    Join and leave

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+from config import TOKEN
+
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send('{} is not connected to a voice channel'.format(ctx.message.author.name))
+        return
+    channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='leave', help='To make the bot leave the voice channel')
+async def leave(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send('The bot is not connected to a voice channel.')
+
+
+if __name__ == '__main__':
+    bot.run(TOKEN)
