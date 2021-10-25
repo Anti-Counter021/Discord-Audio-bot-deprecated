caaece36368dacbfa41a5e7a778daebaba5d3305
counter021 <ara123rus123ara@gmail.com>
2021-10-25 15:56:35 +0300

    Join and leave

--- bot.py
+++ bot.py
@@ -11,8 +11,14 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 @bot.command(name='join', help='Tells the bot to join the voice channel')
 async def join(ctx):
     if not ctx.message.author.voice:
-        await ctx.send('{} is not connected to a voice channel'.format(ctx.message.author.name))
+        await ctx.send(f'{ctx.message.author.name} is not connected to a voice channel')
         return
+
+    voice_client = ctx.message.guild.voice_client
+    if voice_client is not None:
+        await ctx.send('The bot already in voice channel')
+        return
+
     channel = ctx.message.author.voice.channel
     await channel.connect()
 
@@ -20,10 +26,12 @@ async def join(ctx):
 @bot.command(name='leave', help='To make the bot leave the voice channel')
 async def leave(ctx):
     voice_client = ctx.message.guild.voice_client
-    if voice_client.is_connected():
+    if voice_client is None:
+        await ctx.send('The bot is not connected to a voice channel.')
+    elif voice_client.is_connected():
         await voice_client.disconnect()
     else:
-        await ctx.send('The bot is not connected to a voice channel.')
+        await ctx.send('The bot is not connected to a voice channel')
 
 
 if __name__ == '__main__':
